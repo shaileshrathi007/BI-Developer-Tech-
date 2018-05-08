1cc5ad30dfe37302e7537520bf767c0c9fe5b2eb
shaileshrathi007 <33062707+shaileshrathi007@users.noreply.github.com>
2018-05-08 20:14:25 +0100

    delete
    All

--- BI_developer_challenge/generate_series.sql
+++
@@ -1,7 +0,0 @@
-select t::timestamp,count(a.publish)'publish' from generate_series(
-'2018-01-01'::Timestamp,
-'2018-12-31'::Timestamp,
-'1 day' :: interval
-) t left join ads a ON t.timestamp = a.publish AT TIME ZONE 'GMT'
-and to_date(a.publish,'YYYY-MM-DD') > to_date(a.delete,'YYYY-MM-DD') 
-group by publish;
